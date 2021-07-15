ea50235c3db1280dd778521bee34328ef54b0305
andreadangiolillo <82584709+andreadangiolillo@users.noreply.github.com>
2021-07-15 15:21:02 +0200

    Add files via upload

--- smoothing.py
+++ smoothing.py
@@ -1,21 +1,23 @@
 import sys
 
 
+# Function to calculate single exponential smoothing
 def exponential_smoothing(data, alpha):
     result = [data[0]]
     for i in range(1, len(data)):
         result.append(alpha * data[i] + (1 - alpha) * result[i - 1])
-    result.append(alpha * data[-1] + (1 - alpha) * result[-1]) #prediction
+    result.append(alpha * data[-1] + (1 - alpha) * result[-1])  # prediction
     return result
 
 
+# Function to calculate double exponential smoothing
 def double_exponential_smoothing(data, alpha, beta):
     result = [data[0]]
     for i in range(1, len(data) + 2):
         if i == 1:
             level, trend = data[0], data[1] - data[0]
         if i >= len(data):
-            value = result[-1] #prediction
+            value = result[-1]  # prediction
         else:
             value = data[i]
         last_level = level
@@ -24,6 +26,8 @@ def double_exponential_smoothing(data, alpha, beta):
         result.append(level + trend)
     return result
 
+
+# Function to calculate sse
 def sse(values, predictions):
     try:
         s = 0
@@ -32,5 +36,3 @@ def sse(values, predictions):
         return s
     except OverflowError:
         return sys.float_info.max
-
-
--- utilities.py
+++ utilities.py
@@ -1,4 +1,7 @@
-import json, requests, time
+import json
+import requests
+import time
+
 
 def read_json_file(filename):
     try:
@@ -8,18 +11,22 @@ def read_json_file(filename):
     except FileNotFoundError:
         return []
 
-def read_from_prometheus():
 
+def read_from_prometheus():
     try:
-        runtime_json = requests.get("http://localhost:9090/api/v1/status/runtimeinfo")  # ottengo le informazioni di runtime da prometheus
+        runtime_json = requests.get(
+            "http://localhost:9090/api/v1/status/runtimeinfo")  # ottengo le informazioni di runtime da prometheus
         runtime_info = json.loads(runtime_json.text)  # parsing informazioni
         start_time = runtime_info['data']['startTime']  # tempo da cui inizierò a richiedere dati
         end_time = time.time()
-        response = requests.get('http://localhost:9090/api/v1/query_range?query=probe_http_duration_seconds&start=' + start_time + '&end=' + end_time.__str__() + '&step=10m')
+        response = requests.get(
+            'http://localhost:9090/api/v1/query_range?query=probe_http_duration_seconds&start=' +
+            start_time + '&end=' + end_time.__str__() + '&step=10m')
         data = json.loads(response.text)
-    except requests.exceptions.InvalidURL as e:
+    except requests.exceptions.ConnectionError:
         data = []
     return data
+
 
 def get_metrics(data):
     list_metrics = []
